cb22d3d3b2d9eb50f6cddf0451d5a68f7aeaec10
bdolor <bpayne@bccampus.ca>
2018-06-13 10:28:11 -0700

    add new resources content to corresponding tab

--- templates/homepage-template.php
+++ templates/homepage-template.php
@@ -3,6 +3,7 @@
  * Early-Years Theme: homepage template
  *
  * Modified from original header template in cbox theme
+ *
  * @author Brad Payne
  * @package early-years
  * @since 0.9
@@ -19,7 +20,7 @@ infinity_get_header();
 <div class="c-search">
     <h2 class="text-green text-center">Search for learning events</h2>
     <p class="text-center">Fill in one or more of the fields below</p>
-	<?php echo do_shortcode( '[events_search]' ); ?>
+    <?php echo do_shortcode( '[events_search]' ); ?>
 </div>
 <div id="content" role="main" class="sixteen columns">
     <?php
@@ -37,35 +38,36 @@ infinity_get_header();
 </div>
 <div class="c-map row">
     <h2 class="text-green text-center">Find learning events near you</h2>
-    <div class="six columns">
+    <div class="sixteen columns">
 
         <!-- tabs start -->
-        <div id="tabs" class="ui-tabs ui-corner-all ui-widget ui-widget-content">
+        <div id="tabs"
+             class="ui-tabs ui-corner-all ui-widget ui-widget-content">
 
             <ul role="tablist"
                 class="ui-tabs-nav ui-corner-all ui-helper-reset ui-helper-clearfix ui-widget-header">
                 <li role="tab" tabindex="1">
                     <a href="#tabs-1" role="presentation" tabindex="-1"
-                        class="ui-tabs-anchor" id="ui-id-1">Upcoming Events</a>
+                       class="ui-tabs-anchor" id="ui-id-1">Upcoming Events</a>
                 </li>
                 <li role="tab" tabindex="2" aria-controls="tabs-2">
                     <a href="#tabs-2" role="presentation" tabindex="0"
-                        class="ui-tabs-anchor" id="ui-id-2">Recently Posted</a>
+                       class="ui-tabs-anchor" id="ui-id-2">Recently Posted</a>
                 </li>
                 <li role="tab" tabindex="3" aria-controls="tabs-3">
                     <a href="#tabs-3" role="presentation" tabindex="1"
-                        class="ui-tabs-anchor" id="ui-id-3">New Resources</a>
+                       class="ui-tabs-anchor" id="ui-id-3">New Resources</a>
                 </li>
                 <li role="tab" tabindex="4" aria-controls="tabs-4">
                     <a href="#tabs-3" role="presentation" tabindex="2"
-                        class="ui-tabs-anchor" id="ui-id-4">New Events</a>
+                       class="ui-tabs-anchor" id="ui-id-4">New Events</a>
                 </li>
             </ul>
             <div id="tabs-1">
-				<?php
-				$events_list = '[events_list scope="after-today" limit="4"]';
-				echo do_shortcode( $events_list );
-				?>
+                <?php
+                $events_list = '[events_list scope="after-today" limit="4"]';
+                echo do_shortcode( $events_list );
+                ?>
             </div>
             <div id="tabs-2">
                 <?php
@@ -75,8 +77,47 @@ infinity_get_header();
                 ?>
             </div>
             <div id="tabs-3">
+                <?php
+                global $post;
+                $args    = [
+                    'posts_per_page' => 4,
+                    'category_name'  => 'Resources',
+                    'post_status'    => 'publish',
+                    'order'          => 'DESC',
+                ];
+                $myposts = get_posts( $args );
 
-            </div>
+                echo '<div class="table-responsive"><table class="table">';
+
+                foreach ( $myposts as $post ) : setup_postdata( $post );
+                    $child_theme_uri = get_stylesheet_directory_uri();
+                    ?>
+                    <thead>
+                    <tr>
+                        <th>Date/Time</th>
+                        <th>Title</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><?php the_date(); ?></td>
+                        <td>
+                            <p>
+                                <a href="<?php the_permalink(); ?>"
+                                   rel="bookmark"
+                                   title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                            </p>
+                        </td>
+
+                    </tr>
+
+                    </tbody>
+                <?php endforeach;
+
+                echo '</table></div>';
+
+                wp_reset_postdata(); ?>
+            </div> <!-- tab 3 end -->
             <div id="tabs-4">
 
             </div>
