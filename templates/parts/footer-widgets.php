39c05ac816444fe9e9d74193aa05356dd855c5d5
bdolor <brad@bradpayne.ca>
2018-12-17 11:13:14 -0800

    remove hardcoded text

--- templates/parts/footer-widgets.php
+++ templates/parts/footer-widgets.php
@@ -14,26 +14,23 @@
 ?>
 <?php if ( is_active_sidebar( 'Footer Left' ) || is_active_sidebar( 'Footer Middle' ) || is_active_sidebar( 'Footer Right' ) ) : ?>
 	<div class="footer-widgets d-flex flex-row flex-wrap">
-		<div class="col-md">
-			<p>B.C.'s post-secondary educators can promote and join their colleagues in professional development opportunities for teaching and learning around the province. The <i>Teaching and Learning Professional Development portal (TLPD)</i> is sponsored by BCcampus.</p>
-		</div>
 		<?php if ( is_active_sidebar( 'Footer Left' ) ) : ?>
 			<!-- footer widgets -->
-			<div class="col-md-3" id="footer-widget-left">
+			<div class="col-md-4" id="footer-widget-left">
 				<?php
 				dynamic_sidebar( 'Footer Left' );
 				?>
 			</div>
 		<?php endif; ?>
 		<?php if ( is_active_sidebar( 'Footer Middle' ) ) : ?>
-			<div class="col-md-3" id="footer-widget-middle">
+			<div class="col-md-4" id="footer-widget-middle">
 				<?php
 				dynamic_sidebar( 'Footer Middle' );
 				?>
 			</div>
 		<?php endif; ?>
 		<?php if ( is_active_sidebar( 'Footer Right' ) ) : ?>
-			<div class="col-md-3" id="footer-widget-right">
+			<div class="col-md-4" id="footer-widget-right">
 				<?php
 				dynamic_sidebar( 'Footer Right' );
 				?>
